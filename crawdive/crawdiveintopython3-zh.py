68e8780fce77df272f2a3c587619592443f4b0e0
lxq <lxq@debian-lxq>
2014-07-08 22:00:18 +0800

    将crawdive提交到git进行管理。

---
+++ crawdive/crawdiveintopython3-zh.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# coding=UTF-8
+"""
+练习网络抓取程序，将dive into python3的中文版抓取下来。
+"""
+from BeautifulSoup import BeautifulSoup as bp
+import urllib2
+
+if __name__ == "__main__":
+        
+
