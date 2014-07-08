e0c7cd40b2eb7f87f6eec68390175e8e248342b1
lxq <deerlux@gmail.com>
2014-07-08 22:37:25 +0800

    将beautifulsoup部分改为bs4实现。

--- crawdive/crawdiveintopython3-zh.py
+++ crawdive/crawdiveintopython3-zh.py
@@ -3,17 +3,17 @@
 """
 练习网络抓取程序，将dive into python3的中文版抓取下来。
 """
-from BeautifulSoup import BeautifulSoup as bp
+from bs4 import BeautifulSoup as bp
 import urllib2
 
 if __name__ == "__main__":
     _url = 'http://woodpecker.org.cn/diveintopython3/'
     index_html = urllib2.urlopen(_url).read()
     soup = bp(index_html)
-    hrefs = [link.get('href') for link in soup.findAll('a')]
+    hrefs = [link.get('href') for link in soup.find_all('a')]
     print(hrefs)
     hrefs = [x for x in hrefs if str(x).endswith('.html')]
-    print(hrefs)
+    print('\n'.join(hrefs))
 
 
         
