e625444d1318d461f8e36a589ac673e982a072da
lxq <deerlux@gmail.com>
2014-07-11 23:47:36 +0800

    1、将相关的配置文件提交到GIT管理 2、抓取工作基本已经完成，可以存储在一个特定的目录下 TODO:   练习一下关于一个HTML文件里关于同网站的其他媒体等的抓取。

--- crawdive/crawdiveintopython3-zh.py
+++ crawdive/crawdiveintopython3-zh.py
@@ -3,21 +3,39 @@
 """
 练习网络抓取程序，将dive into python3的中文版抓取下来。
 """
-from bs4 import BeautifulSoup as bp
+from bs4 import BeautifulSoup as BS
 import urllib2
+from ConfigParser import ConfigParser
+import os.path
 
 if __name__ == "__main__":
     _url = 'http://woodpecker.org.cn/diveintopython3/'
-    index_html = urllib2.urlopen(_url).read()
-    soup = bp(index_html)
-    hrefs = [link.get('href') for link in soup.find_all('a')]
-    # print(hrefs)
-    hrefs1 = [str(x) for x in hrefs]
-    # html_filter = lambda x: x.endswith('html')
-    hrefs1 = [x for x in hrefs1 if x.endswith('.html')]
-    print('\n'.join(hrefs1))
     
+    conf = ConfigParser()
+    conf.read('crawdiveintopython3-zh.conf')
+    save_dir = conf.get('main','save_dir')
 
+    index_html = urllib2.urlopen(_url).read()
 
-        
+#   save html to file index.html
+    file1 = open(os.path.join(save_dir, 'index.html'), 'w')
+    file1.write(index_html)
+    file1.close()
+
+#   parse the html file
+    soup = BS(index_html)
+
+    hrefs = [str(link.get('href')) for link in soup.find_all('a')]
+    hrefs_file = [x for x in hrefs if x.endswith('.html')]
+    #print('\n'.join(hrefs_file))
+
+    for x in hrefs_file:
+        html = urllib2.urlopen(_url + x).read()
+
+        file1 = open(os.path.join(save_dir, x),'w')
+        file1.write(html)
+        file1.close()
+
+        print("%s\t\t file writing finished!" % x)
+
 
