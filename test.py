25c1a0fa1ca172ed754c4e98731ce2f6f8bb6bcb
lxq <deerlux@gmail.com>
2014-08-09 01:38:55 +0800

    test.py is used to call the fether and DBAPI.

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import sys
 
 from OurkuFundsInfoFetcher import OurkuFundsInfoFetcher
 from FundsDataDB import FundsDataDB
+from sqlalchemy.exc import IntegrityError
 
 
 if __name__ == "__main__":
@@ -25,7 +26,57 @@ if __name__ == "__main__":
     
     db = FundsDataDB(user = db_user, password = db_pass, db_name = db_name,
             host = db_host, dbms = dbms)
+    
+    # get the iterator 
+    result = db.query(db.FundsList.fund_code).\
+            values(db.FundsList.fund_code)
 
-    funds_code = db.session.query(db.FundsList.fund_code).all()
-    print('\n'.join([x[0] for x in funds_code[0:20] ]))
+    fetcher = OurkuFundsInfoFetcher()
+
+    count = 0
+    for i, v in enumerate(result):
+        print('Starting fetch the information of fund "%s"...' % v.fund_code)
+        try:
+            # fetch the basic fund information
+            fetcher.init(v.fund_code)
+            basic_info = fetcher.get_fund_basic()
+            type_query = db.query(db.FundsType.type_id).filter_by(
+                    type_name = basic_info['fund_type']).first()
+
+            # insert funds_type when the type is no exists.
+            if type_query == None:
+                try:
+                    item = db.FundsType(type_name = basic_info['fund_type'])
+                    db.add(item)
+                    db.commit()
+                    type_id = db.query(db.FundsType.type_id).\
+                        filter_by(type_name = basic_info['fund_type']).\
+                        first().type_id
+                    print(u'Added a new type : %s' % basic_info['fund_type'])
+                except IntegrityError as e:
+                    print('Inserting error %s' % v.fund_code)
+                    print(e)
+                    db.rollback()
+                    db.session.close()
+                    type_id = None
+            else:
+                type_id = type_query.type_id
+            print('Type id is: %d', type_id)
+
+            row = db.query(db.FundsList).\
+                    filter_by(fund_code=v.fund_code)
+            
+            row.update({db.FundsList.type_id:type_id,
+                db.FundsList.fund_origin_date:basic_info['origin_date']})
+            count += 1
+        except IndexError as ie:
+            print('fetch data failed: %s' % v.fund_code)
+            print ie
+
+    print('%d records is updated' % count)
+#        if i == 10:
+#            break
+
+    db.commit()
+#    print('\n'.join([x[0] for x in funds_code[0:20] ]))
 
