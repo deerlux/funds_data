2e479b1f0302ceaa8224e532d4e37eb672dc3869
deerlux <deerlux@gmail.com>
2015-01-05 22:18:39 +0800

    完成gewara数据的提取以及显示

--- film_view.py
+++ film_view.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from __future__ import print_function
+
 import matplotlib.pyplot as plt
 import pandas as pd
 from pandas.io.excel import read_excel
-
 import numpy as np
 
 import matplotlib
@@ -13,7 +14,7 @@ def baidu_index(filename, pub_date = None):
     try:
         temp = read_excel(filename)
     except IOError as msg:
-        print e
+        print(e)
         return None
     
     ds = [x.date() for x in temp['收集时间']]
@@ -33,7 +34,7 @@ def douban_comment(filename, pub_date=None):
     try:
         temp = read_excel(filename)
     except IOError as e:
-        print e
+        print(e)
         return None
     
     ds = [x.date() for x in temp['收集时间']]
@@ -51,6 +52,29 @@ def douban_comment(filename, pub_date=None):
     return douban_f2
 
 
+def gewara_data(filename, pub_date):
+    try:
+        temp = read_excel(filename)
+    except IOError as e:
+        print(e)
+        return None
+
+    ds = [x.date() for x in temp['收集时间']]
+    temp.index = ds
+
+    gewara_f = pd.DataFrame({'喜欢人数增量':temp['喜欢人数增量'], 
+        '关注人数增量':temp['关注人数增量'], 
+        '购买人数增量':temp['购买人数增量']})
+
+    gewara_f = gewara_f.groupby(level=0).max()
+
+    if pub_date:
+        delta_days = [x.days for x in gewara_f.index.values 
+                - np.repeat(pub_date, len(gewara_f.index))]
+        gewara_f.index = delta_days
+
+    return gewara_f
+
 if __name__ == '__main__':
     """如果要显示中文，则修改mpl_data/XXXrc文件，将family以及sans-serif相关的内
     容改一下"""
@@ -66,16 +90,34 @@ if __name__ == '__main__':
     ax.plot(baidu_f2.index, baidu_f2['匆匆那年'].values,label ='百度搜索指数',
             linewidth = 2.5)
     ax.set_xlabel('与上映首日间隔')
-    ax.set_xlim(0, 20)
-    ax.set_yscale('log')
+
 
     douban_f2 = douban_comment('douban_film_score.xls', 
             pd.datetime(2014,12,5).date())
 
     ax.plot(douban_f2.index, douban_f2['匆匆那年'].values, 
-            label = '豆瓣评价增量')
+            label = '豆瓣评价增量', linewidth = 2.5)
+
+    gewara_f = gewara_data('gewara.xls', pd.datetime(2014,12,5).date())
+    ax.plot(gewara_f.index, 
+            gewara_f['喜欢人数增量'].values, label='gewara喜欢人数增量',
+            linewidth = 2.5)
+    ax.plot(gewara_f.index, 
+            gewara_f['关注人数增量'].values, label='gewara关注人数增量',
+            linewidth = 2.5)
+    ax.plot(gewara_f.index,
+            gewara_f['购买人数增量'].values, label='gewara购买人数增量',
+            linewidth = 2.5)
+#    gewara_f.plot(ax=ax, linewidth = 2.5)
+    ax.set_xlim(0,20)
     ax.legend(loc='best')
     ax.grid('on')
+    ax.set_title('匆匆那年')
+    ax.set_yscale('log')
     
+    fig.savefig('匆匆那年_log.png')
+    
+
+   
     plt.show()
 
